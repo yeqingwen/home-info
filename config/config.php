ac62d4f89cab1fb9052930b210e17337b33a2922
yeqingwen <317811448@qq.com>
2017-07-29 16:55:10 +0800

    调整配置信息

--- config/config.php
+++ config/config.php
@@ -1,3 +1,11 @@
 <?php
 
 define('INFO_URL', 'http://xxxxx');
+
+function filterStr($str) {
+    $str = str_replace("\n" , '', $str);
+    $str = str_replace(" " , '', $str);
+    $str = str_replace("'" , '', $str);
+    $str = str_replace('"' , '', $str);
+    return str_replace("\r" , '', $str);
+}
