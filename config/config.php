617a172d56dbc0b6390afa16388181b0989c81d1
yeqingwen <317811448@qq.com>
2017-08-02 10:33:15 +0800

    调整部分信息

--- config/config.php
+++ config/config.php
@@ -4,7 +4,6 @@ define('INFO_URL', 'http://xxxxx');
 
 function filterStr($str) {
     $str = str_replace("\n" , '', $str);
-    $str = str_replace(" " , '', $str);
     $str = str_replace("'" , '', $str);
     $str = str_replace('"' , '', $str);
     return str_replace("\r" , '', $str);
