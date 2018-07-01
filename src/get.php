9b07e0f62d712011f36a9070c36247825253ea9f
yeqingwen <317811448@qq.com>
2018-07-01 12:23:32 +0800

    update

--- src/get.php
+++ src/get.php
@@ -2,12 +2,15 @@
 require_once('../lib/Meter.php');
 require_once('../lib/User.php');
 
+define('MAX_USER', 1800);
+define('MAX_METER', 2078);
+
 function index() {
-    User::getAllUser('../output/user.txt', 1, 1763);
-    User::getAllMeter('../output/meter.txt', 1, 1763);
-    Meter::getAllMeterInfo('../output/meter_info.txt', 1, 2072);
-    Meter::getAllMeterHistory('../output/meter_history_info.txt', 1, 2072);
-    Meter::getAllMeterCharge('../output/meter_charge_info.txt', 1, 2072);
+    User::getAllUser('../output/user.txt', 1, MAX_USER);
+    User::getAllMeter('../output/meter.txt', 1, MAX_USER);
+    Meter::getAllMeterInfo('../output/meter_info.txt', 1, MAX_METER);
+    Meter::getAllMeterHistory('../output/meter_history_info.txt', 1, MAX_METER);
+    Meter::getAllMeterCharge('../output/meter_charge_info.txt', 1, MAX_METER);
 }
 
 index();
